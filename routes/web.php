4fd2a880505d945cb1f9c117a851df3640a61e00
unknown <garoaccount@hotmail.com>
2025-08-12 12:54:49 -0400

    added to main branch

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,15 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\MembershipController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth'])->group(function () {
+    Route::resource('appointments', AppointmentController::class);
+    Route::resource('memberships', MembershipController::class);
 });
 
 require __DIR__.'/auth.php';
