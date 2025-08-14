4c018a4730e3c5af7247886d96e4b01259ac7583
unknown <garoaccount@hotmail.com>
2025-08-13 20:25:33 -0400

    fixed code

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     use HasFactory;
-    protected $fillable = ['user_id', 'appointment_time', 'status'];
+    protected $fillable = ['user_id','coach_id', 'appointment_time', 'status'];
 
     public function user() {
         return $this->belongsTo(User::class);
